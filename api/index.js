752c53ca62acb8df21f5defe43ed6d369fd55c34
Umar <surazeeshan.droid@gmail.com>
2025-12-16 11:35:40 -0600

    connect to databass

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() =>{
+    console.log('Connected to MongoDB!');
+})
+.catch((err) =>{
+  console.log(err);
+});
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('API server is running on http://localhost:3000');
+});
