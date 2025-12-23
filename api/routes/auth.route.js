ca7893ed92facfe80606ec00e45eca1ea86d6593
Umar <surazeeshan.droid@gmail.com>
2025-12-22 19:16:39 -0600

    create sign in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { signup } from '../controllers/auth.Controller.js';
+import { signin } from '../controllers/auth.Controller.js';
 
 const router = express.Router();
 router.post("/signup", signup);
-
+router.post("/signin", signin);
 
 export default router;
