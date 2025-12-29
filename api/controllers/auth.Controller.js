4ca427783eb43ef103fdd75d9367c0edb981bf84
Umar <surazeeshan.droid@gmail.com>
2025-12-28 20:12:58 -0600

    complete sign in

--- api/controllers/auth.Controller.js
+++ api/controllers/auth.Controller.js
@@ -5,8 +5,8 @@ import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res,next) => {
     const {username ,email,password} = req.body;
-    const hashedPassword = await bcryptjs.hashSync(password, 10);
-    const newUser = new User ({username,email,hashedPassword});
+    const hashedPassword =  await bcryptjs.hashSync(password, 10);
+    const newUser = new User ({username,email,password});
     try{
         await newUser.save();
         res.status(201).json('User created successfully!');
