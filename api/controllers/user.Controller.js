5652295bee770154954e22108b89f96d37cd5124
Ume Tabassum <surazeeshan@gmail.com>
2026-02-02 12:57:58 +0500

    create get user listings API route

--- api/controllers/user.Controller.js
+++ api/controllers/user.Controller.js
@@ -45,3 +45,18 @@ export const deleteUser = async (req, res, next) => {
         next(err);
     }
 };
+
+export const getUserListings = async (req, res, next) => {
+    if(req.user.id !=== req.params.id) {
+    try {
+        const listings = await Listing.find({userRef: req.params.id});
+        res.status(200).json(listings);
+    } catch (err) {
+        next(err);
+    }
+
+    }else {  
+        return next(errorHandler(401, 'You can view only your own listings'));    
+
+    }
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { test } from '../controllers/user.Controller.js';
-
+import { deleteUser, test, updateUser,getUserListings } from '../controllers/user.Controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 router.get('/test',test); 
 router.post('/update/:id', verifyToken, updateUser);
 router.delete('/delete/:id', verifyToken, deleteUser);
+router.get('/listing/:id', verifyToken, getUserListings);
    
 
 
